6174acf347d5c3d1888c7f14eec8264271e4bad7
madhouse <marvin-hinkley-vortx@gmail.com>
2021-04-14 13:38:43 -0700

    added skin thing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../scss/index.scss"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+  </Layout>
+)
+
+export default IndexPage
