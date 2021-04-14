2f0ea16f0ea289d2e4bce593cea0fc99b68703d8
madhouse <marvin-hinkley-vortx@gmail.com>
2021-04-14 10:35:59 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL || "http://localhost:1337",
+        contentTypes: ["article", "category", "author"],
+        singleTypes: [`homepage`, `global`],
+        queryLimit: 1000,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
 }
